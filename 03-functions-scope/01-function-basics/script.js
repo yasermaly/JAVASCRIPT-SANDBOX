c8d3bdddc2d1cf32ab39537f86bc3d822188dbfb
Yaser Aly <yaser-aly@live.com>
2023-08-08 01:19:27 -0700

    completed lesson 03-01 function basics

---
+++ 03-functions-scope/01-function-basics/script.js
@@ -0,0 +1,19 @@
+function sayHello() {
+  console.log('Hello World!');
+}
+
+sayHello();
+
+function add(num1, num2) {
+  console.log(num1 + num2);
+}
+
+add(5, 10);
+
+function subtract(num1, num2) {
+  return num1 - num2; // return exits the function.
+}
+
+const result = subtract(10, 2);
+
+console.log(result, subtract(20, 5));
