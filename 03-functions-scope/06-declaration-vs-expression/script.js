fd49b8836e079198361a5e81820968974f689156
Yaser Aly <yaser-aly@live.com>
2023-08-08 17:53:00 -0700

    completed lesson 03-06 declaration vs expression

---
+++ 03-functions-scope/06-declaration-vs-expression/script.js
@@ -0,0 +1,15 @@
+// Function Declaration:
+
+function addDollarSign(value) {
+  return '$' + value;
+}
+
+console.log(addDollarSign(100)); // returns $100
+
+// Function Expression:
+
+const addPlusSign = function (value) {
+  return '+' + value;
+};
+
+console.log(addPlusSign(200)); // returns $200
