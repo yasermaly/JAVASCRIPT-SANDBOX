205b068cf80c4cf2c78e472bfcbde294409d6dd1
Yaser Aly <yaser-aly@live.com>
2023-08-15 10:49:19 -0700

    completed lesson 06-01 DOM intro

---
+++ 06-document-object-model/01-what-is-the-dom/script.js
@@ -0,0 +1,12 @@
+// What is the Document Object Model?
+
+// It's a programming interface for web/HTML elements.
+// It's a structure that we can interact with using JavaScript.
+// It includes tags, attributes, text nodes etc.
+// It's represented as a tree structure
+
+console.log(window);
+console.log(document);
+console.log(document.body.innerHTML);
+console.log(document.body.innerText);
+console.log(document.links[0]);
