ffa4d1ea9615807b77207f07ed746c431ac5a386
Yaser Aly <yaser-aly@live.com>
2023-08-18 16:15:02 -0700

    completed lesson 07-02 mouse events

--- 07-events/02-mouse-events/shopping-list/script.js
+++ 07-events/02-mouse-events/shopping-list/script.js
@@ -1,1 +1,38 @@
+const logo = document.querySelector('img');
 
+// functions:
+
+const onClick = () => console.log('click event');
+
+const onDoubleClick = () => {
+  if (document.body.style.backgroundColor !== 'purple') {
+    document.body.style.backgroundColor = 'purple';
+    document.body.style.color = 'white';
+  } else {
+    document.body.style.backgroundColor = 'white';
+    document.body.style.color = 'black';
+  }
+};
+
+const onRightClick = () => console.log('right click event');
+const onMouseDown = () => console.log('mouse down event');
+const onMouseUp = () => console.log('mouse up event');
+const onMouseWheel = () => console.log('mouse Wheel event');
+const onMouseOver = () => console.log('mouse over event');
+const onMouseOut = () => console.log('mouse out event');
+const onDragStart = () => console.log('drag start event');
+const onDragEnd = () => console.log('drag end event');
+const onDrag = () => console.log('drag event');
+
+// Event Listeners
+logo.addEventListener('click', onClick);
+logo.addEventListener('dblclick', onDoubleClick);
+logo.addEventListener('contextmenu', onRightClick);
+logo.addEventListener('mousedown', onMouseDown);
+logo.addEventListener('mouseup', onMouseUp);
+logo.addEventListener('wheel', onMouseWheel);
+logo.addEventListener('mouseover', onMouseOver);
+logo.addEventListener('mouseout', onMouseOut);
+logo.addEventListener('dragstart', onDragStart);
+logo.addEventListener('dragend', onDragEnd);
+logo.addEventListener('drag', onDrag);
