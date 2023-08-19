2cc398dcd6c4f0fabdd5ad1d8ced9ccec52b856a
Yaser Aly <yaser-aly@live.com>
2023-08-19 08:24:31 -0700

    completed lesson 07-09 event delegation

---
+++ 07-events/09-event-delegation/shopping-list/script.js
@@ -0,0 +1,20 @@
+const listItems = document.querySelectorAll('li');
+const list = document.querySelector('ul');
+
+// listItems.forEach((item) => {
+//   item.addEventListener('click', (e) => {
+//     e.target.remove();
+//   });
+// });
+
+list.addEventListener('click', (e) => {
+  if (e.target.tagName === 'LI') {
+    e.target.remove();
+  }
+});
+
+list.addEventListener('mouseover', (e) => {
+  if (e.target.tagName === 'LI') {
+    e.target.style.color = 'red';
+  }
+});
