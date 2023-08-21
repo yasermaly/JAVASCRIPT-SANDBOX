0a08d6b318ac620bc3a63656e479462a5a7a1b8b
Yaser Aly <yaser-aly@live.com>
2023-08-21 16:45:33 -0700

    completed lesson 09-03 callbacks

--- 09-asynchronous-javascript/03-callbacks/script.js
+++ 09-asynchronous-javascript/03-callbacks/script.js
@@ -4,3 +4,27 @@ function toggle(e) {
 }
 
 document.querySelector('button').addEventListener('click', toggle);
+
+const posts = [
+  { title: 'Post One', body: 'This is post one' },
+  { title: 'Post Two', body: 'This is post Two' },
+];
+
+function createPost(post, cb) {
+  setTimeout(() => {
+    posts.push(post);
+    cb();
+  }, 2000);
+}
+
+function getPosts() {
+  setTimeout(() => {
+    posts.forEach(function (post) {
+      const div = document.createElement('div');
+      div.innerHTML = `<strong>${post.title}</strong> - ${post.body}`;
+      document.querySelector('#posts').appendChild(div);
+    });
+  }, 1000);
+}
+
+createPost({ title: 'Post Three', body: 'This is post three' }, getPosts);
