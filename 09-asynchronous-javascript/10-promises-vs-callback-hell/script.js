332e918a4c7b818fd23cf16f138b43a7f0cf3788
Yaser Aly <yaser-aly@live.com>
2023-08-23 10:22:39 -0700

    completed lesson 09-10 promises vs callback hell

--- 09-asynchronous-javascript/10-promises-vs-callback-hell/script.js
+++ 09-asynchronous-javascript/10-promises-vs-callback-hell/script.js
@@ -1,25 +1,36 @@
-function getData(endpoint, cb) {
-  const xhr = new XMLHttpRequest();
+function getData(endpoint) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
 
-  xhr.open('GET', endpoint);
+    xhr.open('GET', endpoint);
 
-  xhr.onreadystatechange = function () {
-    if (this.readyState === 4 && this.status === 200) {
-      cb(JSON.parse(this.responseText));
-    }
-  };
-
-  setTimeout(() => {
-    xhr.send();
-  }, Math.floor(Math.random() * 3000) + 1000);
+    xhr.onreadystatechange = function () {
+      if (this.readyState === 4) {
+        if (this.status === 200) {
+          resolve(JSON.parse(this.responseText));
+        } else {
+          reject('Something went wrong');
+        }
+      }
+    };
+    setTimeout(() => {
+      xhr.send();
+    }, Math.floor(Math.random() * 3000) + 1000);
+  });
 }
 
-getData('./movies.json', (data) => {
-  console.log(data);
-  getData('./actors.json', (data) => {
-    console.log(data);
-    getData('./directors.json', (data) => {
-      console.log(data);
-    });
+getData('./movies.json')
+  .then((movies) => {
+    console.log(movies);
+    return getData('./actors.json');
+  })
+  .then((actors) => {
+    console.log(actors);
+    return getData('./directors.json');
+  })
+  .then((directors) => {
+    console.log(directors);
+  })
+  .catch((error) => {
+    console.log(error);
   });
-});
