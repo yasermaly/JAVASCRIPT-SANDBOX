c9cafa8fa969d1b69bf3e98450c25a58ecd82f8f
Yaser Aly <yaser-aly@live.com>
2023-08-22 21:16:03 -0700

    completed lesson 09-08 callback to promise refactor

--- 09-asynchronous-javascript/08-callback-to-promise/script.js
+++ 09-asynchronous-javascript/08-callback-to-promise/script.js
@@ -1,14 +1,20 @@
-
 const posts = [
   { title: 'Post One', body: 'This is post one' },
   { title: 'Post Two', body: 'This is post two' },
 ];
 
-function createPost(post, cb) {
-  setTimeout(() => {
-    posts.push(post);
-    cb();
-  }, 2000);
+function createPost(post) {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      let error = false;
+      if (!error) {
+        posts.push(post);
+        resolve();
+      } else {
+        reject('Something went wrong');
+      }
+    }, 2000);
+  });
 }
 
 function getPosts() {
@@ -21,4 +27,12 @@ function getPosts() {
   }, 1000);
 }
 
-createPost({ title: 'Post Three', body: 'This is post' }, getPosts);
+function showError(error) {
+  const h3 = document.createElement('h3');
+  h3.innerHTML = `<strong>${error}</strong>`;
+  document.getElementById('posts').appendChild(h3);
+}
+
+createPost({ title: 'Post Three', body: 'This is post' })
+  .then(getPosts)
+  .catch(showError);
