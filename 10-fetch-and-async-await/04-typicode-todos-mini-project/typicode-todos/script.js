25f781d484e9d22a9a11cee056afffda997ec7ca
Yaser Aly <yaser-aly@live.com>
2023-08-24 12:36:34 -0700

    completed lesson 10-04 typicode todos mini project part 2

--- 10-fetch-and-async-await/04-typicode-todos-mini-project/typicode-todos/script.js
+++ 10-fetch-and-async-await/04-typicode-todos-mini-project/typicode-todos/script.js
@@ -1,7 +1,7 @@
 const apiUrl = 'https://jsonplaceholder.typicode.com/todos';
 
 const getTodos = () => {
-  fetch(apiUrl + '?_limit=5')
+  fetch(apiUrl + '?_limit=10')
     .then((res) => res.json())
     .then((data) => {
       console.log(data);
@@ -11,6 +11,7 @@ const getTodos = () => {
 
 const addTodoToDOM = (todo) => {
   const div = document.createElement('div');
+  div.classList.add('todo');
   div.appendChild(document.createTextNode(todo.title));
   div.setAttribute('data-id', todo.id);
   if (todo.completed) {
@@ -37,9 +38,44 @@ const createTodo = (e) => {
     .then((data) => addTodoToDOM(data));
 };
 
+const toggleCompleted = (e) => {
+  if (e.target.classList.contains('todo')) {
+    e.target.classList.toggle('done');
+    updateTodo(e.target.dataset.id, e.target.classList.contains('done'));
+  }
+};
+
+const updateTodo = (id, completed) => {
+  fetch(`${apiUrl}/${id}`, {
+    method: 'PUT',
+    body: JSON.stringify({ completed }),
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+};
+
+const deleteTodo = (e) => {
+  if (e.target.classList.contains('todo')) {
+    const id = e.target.dataset.id;
+    fetch(`${apiUrl}/${id}`, {
+      method: 'DELETE',
+    })
+      .then((res) => res.json())
+      .then(() => e.target.remove());
+  }
+};
+
 const init = () => {
   document.addEventListener('DOMContentLoaded', getTodos);
+
   document.querySelector('#todo-form').addEventListener('submit', createTodo);
+
+  document
+    .querySelector('#todo-list')
+    .addEventListener('click', toggleCompleted);
+
+  document.querySelector('#todo-list').addEventListener('dblclick', deleteTodo);
 };
 
 init();
