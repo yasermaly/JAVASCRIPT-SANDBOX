bec17fe7a1cfc39370957c181c14c0606a49a2fa
Yaser Aly <yaser-aly@live.com>
2023-08-31 22:57:52 -0700

    completed lesson 12-05 animated clock part 2

--- 12-web-browser-apis/06-animated-clock-2/animated-clock/script.js
+++ 12-web-browser-apis/06-animated-clock-2/animated-clock/script.js
@@ -1,6 +1,12 @@
+const faceColor = document.getElementById('face-color');
+const borderColor = document.getElementById('border-color');
+const lineColor = document.getElementById('line-color');
+const largeHandColor = document.getElementById('large-hand-color');
+const secondHandColor = document.getElementById('second-hand-color');
+const canvas = document.getElementById('canvas');
+
 function clock() {
   const now = new Date();
-  const canvas = document.getElementById('canvas');
   const ctx = canvas.getContext('2d');
 
   // Setup canvas
@@ -19,7 +25,8 @@ function clock() {
   ctx.save();
   ctx.beginPath();
   ctx.lineWidth = 14;
-  ctx.strokeStyle = '#800000';
+  ctx.strokeStyle = borderColor.value;
+  ctx.fillStyle = faceColor.value;
   ctx.arc(0, 0, 142, 0, Math.PI * 2, true);
   ctx.stroke();
   ctx.fill();
@@ -27,6 +34,7 @@ function clock() {
 
   // Draw hour lines
   ctx.save();
+  ctx.strokeStyle = lineColor.value;
   for (let i = 0; i < 12; i++) {
     ctx.beginPath();
     ctx.rotate(Math.PI / 6);
@@ -38,6 +46,7 @@ function clock() {
 
   // Draw minute lines
   ctx.save();
+  ctx.strokeStyle = lineColor.value;
   ctx.lineWidth = 4;
   for (let i = 0; i < 60; i++) {
     if (i % 5 !== 0) {
@@ -62,7 +71,7 @@ function clock() {
   ctx.rotate(
     (Math.PI / 6) * hr + (Math.PI / 360) * min + (Math.PI / 21600) * sec
   );
-  ctx.strokeStyle = '#800000';
+  ctx.strokeStyle = largeHandColor.value;
   ctx.lineWidth = 14;
   ctx.beginPath();
   ctx.moveTo(-20, 0);
@@ -73,7 +82,7 @@ function clock() {
   // Draw min hand
   ctx.save();
   ctx.rotate((Math.PI / 30) * min + (Math.PI / 1800) * sec);
-  ctx.strokeStyle = '#800000';
+  ctx.strokeStyle = largeHandColor.value;
   ctx.lineWidth = 10;
   ctx.beginPath();
   ctx.moveTo(-28, 0);
@@ -84,7 +93,7 @@ function clock() {
   // Draw sec hand
   ctx.save();
   ctx.rotate((sec * Math.PI) / 30);
-  ctx.strokeStyle = '#FF7F50';
+  ctx.strokeStyle = secondHandColor.value;
   ctx.fillStyle = '#FF7F50';
   ctx.lineWidth = 6;
   ctx.beginPath();
@@ -102,3 +111,11 @@ function clock() {
 }
 
 requestAnimationFrame(clock);
+
+document.getElementById('save-btn').addEventListener('click', () => {
+  const dataURL = canvas.toDataURL('img/png');
+  const link = document.createElement('a');
+  link.download = 'clock.png';
+  link.href = dataURL;
+  link.click();
+});
