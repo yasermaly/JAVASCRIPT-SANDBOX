8d4199b4382f1491c6a07ade4944a0d2337a1ca6
Yaser Aly <yaser-aly@live.com>
2023-09-06 14:16:30 -0700

    completed lesson 14-03 static methods

--- 14-oop-classes-private-properties/03-static-methods/script.js
+++ 14-oop-classes-private-properties/03-static-methods/script.js
@@ -4,4 +4,15 @@ class Rectangle {
     this.height = height;
     this.width = width;
   }
+  area() {
+    return this.width * this.height;
+  }
+
+  static getClass() {
+    return 'Rectangle';
+  }
 }
+
+const rect = new Rectangle('rect', 10, 10);
+console.log(rect.area());
+console.log(Rectangle.getClass());
