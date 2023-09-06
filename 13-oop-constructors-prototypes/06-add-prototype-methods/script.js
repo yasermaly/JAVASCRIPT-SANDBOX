fd7369bac809108895dc3199fb98f11c6c653195
Yaser Aly <yaser-aly@live.com>
2023-09-05 21:14:44 -0700

    completed lesson 13-06 adding methods to the prototype

--- 13-oop-constructors-prototypes/06-add-prototype-methods/script.js
+++ 13-oop-constructors-prototypes/06-add-prototype-methods/script.js
@@ -2,12 +2,20 @@ function Rectangle(name, width, height) {
   this.name = name;
   this.width = width;
   this.height = height;
-  this.area = function () {
-  	return this.width * this.height;
-  };
 }
 
+Rectangle.prototype.area = function () {
+  return this.width * this.height;
+};
 
-const rect = new Rectangle('Rect', 10, 10);
+Rectangle.prototype.perimeter = function () {
+  return 2 * (this.width + this.height);
+};
+
+Rectangle.prototype.isSquare = function () {
+  return this.width === this.height;
+};
+
+const rect = new Rectangle('Rect', 30, 10);
 
 console.log(rect);
